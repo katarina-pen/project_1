5c4c826153a51aa9dacbbed469fff9333b2590cb
katarina-pen <katarinapenava0711@gmail.com>
2025-01-23 20:35:26 +0100

    Update code.ino
    Jag har gjort så att hjulen snurrar och har börjat med att försöka implementera rtc modulen.

--- code/code.ino
+++ code/code.ino
@@ -1,9 +1,40 @@
+
+// Include Libraries
+#include <RTClib.h>
+
+//wheels
+int wheel1 = 10;
+int wheel2 = 9;
+//led
+const int ledPin = 8;
+
+// construct objects
+RTC_DS3231 rtc;  
+
 void setup() {
-  // put your setup code here, to run once:
+  
+  rtc.begin();
+  rtc.adjust(DateTime(F(__DATE__), F(__TIME__)));
+  
+  pinMode (wheel1, OUTPUT);
+  pinMode (wheel2, OUTPUT);
+  pinMode (ledPin, OUTPUT);
 
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+wheels ();
+
+}
+
+
+void wheels () {  //Funktion för hjulen
+  digitalWrite (wheel1, HIGH);
+  digitalWrite (wheel2, HIGH);
+  delay (1000);
+  digitalWrite(wheel1, LOW);
+  digitalWrite(wheel2, LOW);
+  delay (1000);
+
 
 }
